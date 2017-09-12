bd66816e0d908a4e5d21d6645634e186f6c5bd30
Tracy1994 <2353912151@qq.com>
2017-09-12 15:27:19 +0800

    variable

---
+++ variable.py
@@ -0,0 +1,18 @@
+a = 1
+print a
+
+a = 123
+print a
+
+b = "hello world"
+print b
+
+DaysPerweek = 7
+HoursPerDay = 24
+MintuesPerHour = 60
+MinPerWeek = DaysPerweek * HoursPerDay * MintuesPerHour
+print "MinPerWeek:", MinPerWeek
+
+HoursPerDay = 26
+MinPerWeek = DaysPerweek * HoursPerDay * MintuesPerHour
+print "26 HourPerDay total mins:", MinPerWeek
