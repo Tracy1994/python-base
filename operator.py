8149e781044445084f7c2a56aa8830efcceff610
Tracy1994 <2353912151@qq.com>
2017-09-15 09:06:43 +0800

    add str.py

---
+++ operator.py
@@ -0,0 +1,18 @@
+
+pricePerPerson = (35.27-0.15) / 3
+print 'pricePerPerson:',pricePerPerson
+
+long = 16.7
+width = 12.5
+area = long * width
+perimeter = (long + width) * 2
+print 'area:', area
+print 'perimeter:', perimeter
+
+print "please input the f value"
+F = 62
+C = ( F - 32)/(9/5)
+print 'C:', C
+
+hours = 200/80
+print 'hours:', hours
---
+++ str.py
@@ -0,0 +1,27 @@
+num1 = 123
+print type(num1)
+
+# this is the section
+num2 = "12345678"
+print type(num2)
+# select the second charter of the str
+print num2[1]
+# select from the first letter(include the first one) to the third letter(not include third one)
+print num2[0:3]
+# select from the first letter(include the first one) to the third letter(not include third one)
+print num2[:3]
+# select from the third letter(include the first one) to the last letter( include third one)
+print num2[3:]
+
+print num2[::1]
+print num2[::2]
+print num2[0]
+print num2[-1]
+print num2[-4:-1]
+print num2[-4:]
+
+num3 = 1234567899999999999999999
+print type(num3)
+
+num4 = 12.3
+print type(num4)
